0d8a526aafaf07e81c461431f4f7a17b7690567a
owusuvictorjnr <owusuvictor80@gmail.com>
2022-09-29 12:14:43 +0100

    service rendering cards

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,6 +8,213 @@ const Main = () => {
       <div className="m-10 p-5 ">
         <div className="mt-[10rem]">
           <h3 className="flex justify-center tracking-widest font-semibold underline uppercase">
+            the service we render
+          </h3>
+        </div>
+
+        <div className="grid lg:grid-cols-5 md:grid-cols-2 m-5   lg:gap-[5rem] mx-10">
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+
+          <div className="shadow-xl w-[20rem] h-[25rem] rounded-md bg-red-300 m-5  p-5">
+            <div className="rounded-full bg-red-500 w-40 h-40 mx-auto overflow-hidden">
+              <Image
+                src="/assets/shopforme1.jpg"
+                alt="me"
+                width="150px"
+                height="150px"
+                layout="responsive"
+                objectFit="cover"
+                className="max-w-lg h-auto rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* customer reviews */}
+      <div className="m-10 p-5 ">
+        <div className="mt-[10rem]">
+          <h3 className="flex justify-center tracking-widest font-semibold underline uppercase">
             customers review
           </h3>
         </div>
